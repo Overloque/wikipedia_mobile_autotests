460e5501abfabb63f4bdb0e3d435e48b9972741b
Overloque <poludnicyn.99@mail.ru>
2023-10-18 22:20:48 +0500

    fix

--- src/test/java/demo/qa/config/MobileConfig.java
+++ src/test/java/demo/qa/config/MobileConfig.java
@@ -8,10 +8,8 @@ public interface MobileConfig extends Config {
     String getApp();
 
     @Key("mobile.${env}.device")
-    @DefaultValue("Samsung Galaxy S22 Ultra")
     String getDevice();
 
     @Key("mobile.${env}.os_version")
-    @DefaultValue("12.0")
     String getVersion();
 }
--- src/test/java/demo/qa/tests/IosTests.java
+++ src/test/java/demo/qa/tests/IosTests.java
@@ -1,25 +1,30 @@
 package demo.qa.tests;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Selenide.$;
-import static com.codeborne.selenide.Selenide.$$;
-import static io.appium.java_client.AppiumBy.accessibilityId;
 import static io.appium.java_client.AppiumBy.id;
 import static io.qameta.allure.Allure.step;
+import static org.assertj.core.api.Assertions.assertThat;
 
 @Tag("ios")
 public class IosTests extends TestBase {
+    @DisplayName("Проверка соответствия введенного текста в поле поиска")
     @Test
     void successfulSearchTest() {
-        step("Type request in search field", () -> {
-
+        step("Нажать на поле поиска", () -> {
+            $(id("Text Button")).click();
         });
 
-        step("Veriy content is not null", () ->
-                $$(id("org.wikipedia.alpha:id/page_list_item_title"))
-                        .shouldHave(sizeGreaterThan(0)));
+        step("Ввести запрос 'test request и нажать на Enter'", () -> {
+            $(id("Text Input")).sendKeys("test request");
+            $(id("Text Input")).pressEnter();
+        });
+
+        step("Проверка введенного текста на соответствие", () -> {
+            assertThat($(id("Text Output")).getText()).isEqualTo("test request");
+        });
     }
 }
